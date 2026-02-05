31126f32ad94af4eab7cf9ff3f6413eace77d412
Nico.Stein <nico.stein@vit-bund.de>
2026-02-05 09:46:22 +0100

    adding comments with paramater

--- Settings/Utils/SettingUtils.lua
+++ Settings/Utils/SettingUtils.lua
@@ -1,32 +1,67 @@
 local _, addon = ...
-SettingUtils = {}
+BetterSettingUtils = {}
+
+-- Register
+function BetterSettingUtils.RegisterDefaultSettings(table)
+    if table ~= nil and addon.Settings == nil then
+        addon.Settings = table
+    end
+end
 
 -- Register the global SavedVariables for the AddOn
-function SettingUtils.RegisterSettingsDatabase(table)
+---@param table table
+function BetterSettingUtils.RegisterSettingsDatabase(table)
     if table ~= nil then
         addon.SettingsDatabase = table
     end
 end
 
 -- Register character specific SavedVariables for the AddOn
-function SettingUtils.RegisterCharacterSettingsDatabase(table)
+---@param table table
+function BetterSettingUtils.RegisterCharacterSettingsDatabase(table)
     if table ~= nil then
         addon.CharacterSettingsDatabase = table
     end
 end
 
+-- Returns the Settings database
+---@param character boolean
+function BetterSettingUtils.GetSettingsDatabase(character)
+    if character == true and addon.CharacterSettingsDatabase then
+        return addon.CharacterSettingsDatabase
+    elseif character == false and addon.SettingsDatabase then
+        return addon.SettingsDatabase
+    end
+end
+
 -- Read a specific Setting from the settings database
-function SettingUtils.GetSettingValue(key, character)
+---@param key string
+---@param character boolean
+function BetterSettingUtils.GetSettingValue(key, character)
     if not key or key == "" then
         return nil
     end
 
-    local db
-    if character then
-        db = addon.CharacterSettingsDatabase
-    else
-        db = addon.SettingsDatabase
-    end
+    local db = BetterSettingUtils.GetSettingsDatabase(character)
 
     return db[key]
+end
+
+-- Create a new AddOn Setting and returning the setting.
+-- Can be a global or character setting.
+---@param category unknown
+---@param key string
+---@param lang { label: string, tooltip: string }
+---@param character boolean?
+function BetterSettingUtils.CreateAddonSetting(category, key, lang, character)
+    local option = addon.Settings[key]
+    local settingDB = BetterSettingUtils.GetSettingsDatabase(character)
+
+    if option ~= nil then
+        local setting = Settings.RegisterAddOnSetting(category, option.key, option.key,
+            settingDB, type(option.default), lang.label, option.default)
+        setting:SetValueChangedCallback(function(s, v) settingDB[s:GetVariable()] = v end)
+
+        return setting
+    end
 end
